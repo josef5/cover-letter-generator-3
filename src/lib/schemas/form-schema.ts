a9fa9abc1946b5aa27cafa33873a05163d76945c
Jose Espejo <joseespejo@yahoo.com>
2025-01-29 12:02:12 +0000

    refactor: remove gpt-4-turbo
    too expensive

--- src/lib/schemas/form-schema.ts
+++ src/lib/schemas/form-schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const mainFormSchema = z.object({
   jobDescription: z.string().min(1, { message: "Required" }),
   salutation: z.string().min(1, { message: "Required" }),
-  model: z.enum(["gpt-3.5-turbo", "gpt-4o", "gpt-4o-mini", "gpt-4-turbo"]),
+  model: z.enum(["gpt-3.5-turbo", "gpt-4o", "gpt-4o-mini"]),
   temperature: z
     .number({ required_error: "Required" })
     .min(0, { message: "Enter a value between 0 and 2.0" })
