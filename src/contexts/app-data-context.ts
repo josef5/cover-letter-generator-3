dec6161f128e62e3ac228dbe8f9c80e81f98cf2e
Jose Espejo <joseespejo@yahoo.com>
2025-02-03 12:43:01 +0000

    refactor: revert jobDescription in default values for app data context

--- src/contexts/app-data-context.ts
+++ src/contexts/app-data-context.ts
@@ -16,9 +16,9 @@ export const AppDataContext = createContext<AppDataContextValue>(
 
 export const defaultValues: FormValues = {
   salutation: "Dear Hiring Manager,",
-  // jobDescription: "",
-  jobDescription:
-    "We are Awesome Co. and we are looking for a Software Engineer to join our team. You will be working on our core product, which is a platform that helps people write better cover letters. You will be responsible for building new features, fixing bugs, and improving the performance of our platform. The ideal candidate is passionate about writing clean code, has experience with React and Node.js, and is a great team player. If you are interested in this position, please send us your resume and a cover letter explaining why you are a good fit for this role.",
+  jobDescription: "",
+  // jobDescription:
+  //   "We are Awesome Co. and we are looking for a Software Engineer to join our team. You will be working on our core product, which is a platform that helps people write better cover letters. You will be responsible for building new features, fixing bugs, and improving the performance of our platform. The ideal candidate is passionate about writing clean code, has experience with React and Node.js, and is a great team player. If you are interested in this position, please send us your resume and a cover letter explaining why you are a good fit for this role.",
   model: "gpt-3.5-turbo",
   temperature: 0.7,
   wordLimit: 300,
