4f05c9cbed5168794e00a4f5573e73608469a06b
Jose Espejo <joseespejo@yahoo.com>
2025-02-03 16:59:00 +0000

    chore: config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/cover-letter-generator-3",
   plugins: [react()],
   resolve: {
     alias: {
